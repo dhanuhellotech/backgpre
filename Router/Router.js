d46a4599840d89adcb23bffba5cc86a2472bcfb3
dhanuhellotech <dhanalakshmihellotech@gmail.com>
2024-03-08 16:12:09 +0530

    first commit

---
+++ Router/Router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const contactController = require('../Controller/Contactus');
+const franschiseController = require('../Controller/Franschise')
+const EnquiryController = require("../Controller/Enquiry")
+const admissionController = require("../Controller/Admission")
+const blogRoutes = require('../Router/blogRoutes');
+const calendarRoutes = require('../Router/calendarRoutes')
+const teacherRoutes = require('../Router/teacherRoutes');
+const newsletterRoutes = require( '../Router/newsletterRoutes')
+const classRoutes = require('../Router/classRoutes')
+const  timetableRoutes= require('../Router/timetableRoutes')
+// const topbarRoutes =require('../Router/tobbarRouter')
+router.post('/contacts', contactController.createContact);
+const nodemailer = require('nodemailer');
+
+router.get('/contacts', contactController.getAll);
+router.get("/contacts/:id", contactController.getbyid);
+router.put("/contacts/:id", contactController.updateById);
+router.delete("/contacts/:id", contactController.deleteById);
+
+//franschise
+router.post("/franschise", franschiseController.franschiseadd);
+router.get("/franschise", franschiseController.getAllfrans);
+router.delete('/franschise/:id', franschiseController.deleteFranschise);
+
+//enquiry
+router.post("/enquiry", EnquiryController.childEnquiry);
+router.get("/enquiry", EnquiryController.enquiryGet);
+router.delete("/enquiry/:id", EnquiryController.deleteAdmissionById);
+
+// //Addmission]
+router.post("/Addmission", admissionController.createAdmission);
+router.get('/Addmission', admissionController.getAllAdmissions);
+router.delete("/Addmission/:id", admissionController.deleteByAdmissionId);
+
+  
+module.exports = router;
